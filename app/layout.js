d88ad7b2d50c3bc2ee9c61e4a99cf5b6db82a868
Ziad Elbadawi <ziad@Ziads-MacBook-Pro.local>
2026-01-28 13:51:27 +0200

    fv

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "react-modal-video/css/modal-video.css"
 import "/public/assets/css/lockdown.css"
 import "/public/assets/css/lockdown-responsive.css"
 import "swiper/css"
-// import "swiper/css/navigation"
 import "swiper/css/pagination"
 import "swiper/css/free-mode"
 import "./responsive-menu.css"
@@ -23,10 +22,6 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${nunito.variable} ${montserratAlternates.variable}`}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="preload" as="style" href="/public/assets/css/lockdown.css" />
-        <link rel="preload" as="style" href="/public/assets/css/lockdown-responsive.css" />
       </head>
       <body>{children}</body>
     </html>
